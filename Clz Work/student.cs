7826d087ecdcc0a5a47c95405037cf33c4b61c19
Yuri9828835868 <135593196+Yuri9828835868@users.noreply.github.com>
2024-11-26 08:03:57 +0545

    First Commit

---
+++ Clz Work/student.cs
@@ -0,0 +1,15 @@
+﻿namespace Clz_Work
+{
+    public class student
+    {
+        public int ID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public string EnrollmentDate { get; set; }
+        public string Address { get; set; }
+        public string CreatedDate { get; set; }
+        public string CreatedBy { get; set; }
+    }
+}
